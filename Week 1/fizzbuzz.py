d75f4306b828d392281344978c76db67ebcc018d
Andesia <lihasigrace@gmail.com>
2022-11-17 14:47:34 +0300

    Update fizzbuzz.py

---
+++ Week 1/fizzbuzz.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        n = answer[i]
+
+        if ((i % 3 == 0) and (i % 5 == 0)):
+            n = "FizzBuzz"
+
+        elif i % 3 == 0:
+            n = "Fizz"
+
+        elif i % 5 == 0:
+            n = "Buzz"
+
+        else:
+            n = "i"
